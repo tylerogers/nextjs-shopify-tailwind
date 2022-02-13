79905c6cb059deaff55399aee26b2457c508284a
David Medero <dmedero@getwrecked.com>
2022-02-13 00:51:20 -0500

    added footer

--- components/Footer.js
+++ components/Footer.js
@@ -11,13 +11,13 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
         <nav className="flex flex-wrap justify-center">
           {
-              navigation.map((item, i) => {
+              navigation.map((item, i) => (
                   <div key={i} className="px-6 py-2">
                       <a href={item.href} className="text-gray-500 hover:text-gray-900">
                         {item.name}
                       </a>
                   </div>
-              })
+                ))
           }
         </nav>
         <p className="mt-8 text-center text-gray-400">&copy; 2022 Bunni Luna. All rights reserved.</p>
