6ae4bf0560292dac84002ba4c83cb2ca36b1af4f
David Medero <dmedero@getwrecked.com>
2022-02-16 03:48:28 -0500

    fixed highlight issue

--- components/ProductOptions.js
+++ components/ProductOptions.js
@@ -4,7 +4,7 @@ export default function ProductOptions({ name, values, selectedOptions, setOptio
   return (
     <fieldset>
         <legend className='text-xl font-semibold pt-4'>{name}</legend>
-        <div className="david inline-flex items-center flex-wrap active:bg-transparent">
+        <div className="highlight-parent inline-flex items-center flex-wrap">
         {
             values.map(value => {
                 const id = `option-${name}-${value}`
@@ -22,7 +22,7 @@ export default function ProductOptions({ name, values, selectedOptions, setOptio
                             setOptions(name, value)
                         }}
                         />
-                        <div className={`yaima p-2 my-3 text-lg rounded-full block cursor-pointer mr-3 ${checked ? "text-white bg-gray-900" : "text-gray-900 bg-gray-200"}`}>
+                        <div className={`highlight-child p-2 my-3 text-lg rounded-full block cursor-pointer mr-3 ${checked ? "text-white bg-gray-900" : "text-gray-900 bg-gray-200"}`}>
                             <span className='px-2'>{value}</span>
                         </div>
                     </label>
